169e9986a7f29e6062f23afa24d551e6cfa25f4d
shiki2014 <shiki2014@outlook.com>
2023-06-29 14:55:22 +0800

    第一次提交

---
+++ src/api/api.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+  baseURL: '/api' // 设置你的后端 API 的基本 URL
+  // 这里可以设置其他的 Axios 配置，例如 headers、timeout 等
+})
+
+export function getUsers () {
+  return apiClient.get('/users')
+}
+
+export function getData () {
+  return apiClient.get('/users')
+}
+
+export function getPositions () {
+  return apiClient.get('/positions')
+}
---
+++ src/views/home/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div>这里显示数据{{ data }}</div>
+    <div>所有仓位数据</div>
+    <div></div>
+  </div>
+</template>
+
+<script>
+import { getUsers, getData } from '@/api/api.js'
+
+export default {
+  data () {
+    return {
+      data: 1
+    }
+  },
+  created () {
+    getData().then(res => {
+      this.data = res.data
+    })
+  },
+  methods: {
+    getData () {
+      getUsers().then(res => {
+        console.log(res)
+        this.data = res.data
+      }).catch()
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
