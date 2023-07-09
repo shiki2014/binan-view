d77baa4f679ac7da072205330d7f52c47316e5e1
shiki2014 <shiki2014@outlook.com>
2023-07-09 18:33:16 +0800

    前端页面更改

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
+import './assets/css/main.css'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
 
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,20 +1,33 @@
 <template>
   <div>
-    <el-row>
-      <el-col :span="16">
-        <el-statistic group-separator="," :precision="2" :value="data.length" title="当前仓位数量"></el-statistic>
+    <div class="header" :class="haveHeader ? 'active':''" @click="changeHeader">
+      <div class="top flexBox">
+        <el-button type="primary" @click="goLogErrorView">查看错误日志</el-button>
+        <el-button type="primary" @click="goLogAppView">查看App日志</el-button>
+        <i class="el-icon-arrow-down" style="margin-left:auto;"></i>
+      </div>
+      <div class="information">
+        <el-row>
+          <el-col :span="24">
+            当前仓位数量: {{ data.length }}
+          </el-col>
+          <el-col :span="24">
+            未实现利润: {{ unrealizedProfitAll }}
+          </el-col>
+        </el-row>
+      </div>
+    </div>
+    <el-row class="content" :class="haveHeader ? 'active':''">
+      <el-col :span="24">
         <el-collapse v-model="activeNames">
           <el-collapse-item v-for="item in data" :key="item.symbol" :title="item.symbol" :name="item.symbol">
             <div>方向: {{ item.positionSide === 'SHORT' ? '空头' : '多头'}}</div>
             <div>均价: {{ item.entryPrice }}</div>
             <div>杠杆: {{ item.leverage }}</div>
+            <div>保证金: {{ item.isolatedWallet }}</div>
             <div>未实现利润: {{ item.unrealizedProfit }}</div>
           </el-collapse-item>
         </el-collapse>
-      </el-col>
-      <el-col :span="8">
-        <div><el-button style="width:100%;" type="primary" @click="goLogErrorView">查看错误日志</el-button></div>
-        <div><el-button style="width:100%;" type="primary" @click="goLogAppView">查看App日志</el-button></div>
       </el-col>
     </el-row>
   </div>
@@ -27,12 +40,19 @@ export default {
   data () {
     return {
       data: [],
-      activeNames: []
+      activeNames: [],
+      unrealizedProfitAll: 0,
+      haveHeader: false
     }
   },
   created () {
     getPositions().then(res => {
       this.data = res.data
+      let num = 0
+      for (let i = 0; i < this.data.length; i++) {
+        num += Number(this.data[i].unrealizedProfit)
+      }
+      this.unrealizedProfitAll = Number(num.toFixed(2))
     })
   },
   methods: {
@@ -43,11 +63,51 @@ export default {
     goLogAppView () {
       // 查看错误日志
       this.$router.push('/appLog')
+    },
+    changeHeader () {
+      this.haveHeader = !this.haveHeader
     }
   }
 }
 </script>
 
-<style>
-
+<style scoped>
+.main{
+  display: flex;
+  width:100vw;
+  height: 100vh;
+  overflow: hidden;
+  flex-direction: column;
+}
+.header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 2;
+  height: 114px;
+  width: 100%;
+  background-color: #fff;
+  box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.15);
+  transition: all 0.3s ease-in-out;
+  padding:10px;
+  overflow: hidden;
+}
+.header.active{
+  height: 60px;
+}
+.content{
+  transition: all 0.3s ease-in-out;
+  height: 100%;
+  margin-top: 114px;
+}
+.content.active{
+  margin-top: 60px;
+}
+.information{
+  transition: all 0.3s ease-in-out;
+  margin-top:10px;
+}
+.header .top{
+  align-items: center;
+}
 </style>
